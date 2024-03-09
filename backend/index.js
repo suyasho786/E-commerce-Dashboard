ddc0c000ad0ce062b807c9ae643e391342a64269
Suyash Mishra <114728873+suyasho786@users.noreply.github.com>
2024-03-09 14:41:47 +0530

    modified nodemodule location

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const Product = require('./db/Product');
 const Jwt = require('jsonwebtoken');
 const jwtKey = 'e-comm-key';
 const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.post('/register', async (req, resp) => {
@@ -123,4 +124,4 @@ function verifyToken (req, res, next) {
     }
 }
 
-app.listen(5000);
+app.listen(PORT);
